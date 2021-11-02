fc21cb6482f440c25d97a865d1da8f25200934e9
Courtney Doss <777.catalyst@gmail.com>
2021-11-02 10:43:42 -0500

    Update db model with needed not nullable attributes

--- model.py
+++ model.py
@@ -10,10 +10,13 @@ class User(db.Model):
     user_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
-    fname = db.Column(db.String)
-    lname = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
+    fname = db.Column(db.String(30), nullable = False)
+    lname = db.Column(db.String(30), nullable = False)
+    email = db.Column(db.String(100), nullable = False, unique=True)
+    password = db.Column(db.String(100), nullable = False)
+
+    #creates relationship and gives access to reviews made by specific user
+    reviews = db.relationship("Reviews", back_populates="user")
 
     def __repr__(self):
         return f'<User user_id={self.user_id}email={self.email}>'
@@ -26,19 +29,23 @@ class Reviews(db.Model):
     review_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
-    cleanliness = db.Column(db.Integer)
+    date_time = db.column(db.DateTime)
+    cleanliness = db.Column(db.Integer, nullable = False)
     accessibile = db.Column(db.Boolean)
     lgbt_friendly = db.Column(db.Boolean)
     comments = db.column(db.Text)
     #TODO find out data type of id pulled from maps api for each bathroom
     bathroom_id = db.Column(db.String, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user_info.user_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user_info.user_id"), nullable = False)
     
-    user = db.relationship("User", backref="ratings_info") #attribute added, returns a list of Ratings
+    #creates relationship and gives access to user that made review
+    user = db.relationship("User", back_populates="reviews")
 
     def __repr__(self):
         return f'<Reviews review_id={self.review_id} cleanliness={self.cleanliness}>'
 
+
+#TODO be sure to call db "reviews"
 def connect_to_db(flask_app, db_uri="postgresql:///reviews", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
@@ -50,7 +57,7 @@ def connect_to_db(flask_app, db_uri="postgresql:///reviews", echo=True):
     print("Connected to the db!")
 
 
-if __name__ == "__main__": #does something so we can use our application
+if __name__ == "__main__": 
     from server import app
 
     # Call connect_to_db(app, echo=False) if your program output gets
