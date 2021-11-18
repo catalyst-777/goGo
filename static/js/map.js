77dc768d13bbdef15550bb576861ae890ce1a81d
Courtney Doss <777.catalyst@gmail.com>
2021-11-18 17:51:10 -0600

    Clean up and comment code

--- static/js/map.js
+++ static/js/map.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// declare necessary variables in the global scope
 let map;
 let userMarker;
 let restroomLocation;
@@ -7,22 +9,27 @@ let restroomLatLng;
 let restroomLat;
 let restroomLng;
 
+// overall function that initializes map object
 function initMap() {
+
+  //define object to store a default user lat/lng, in case geo location permission not given. 
   const userLoc = {
     lat: 40.000,
     lng: -79.000
   }
 
+  //options object stores where map should center and how far in to zoom
   const options = {
     center: userLoc,
     zoom: 15
   }
-  if(navigator.geolocation) {
-    console.log('geolocation is here!');
 
-    //this will ask user for permission to get their position
+  // checks if geo location is supported
+  if(navigator.geolocation) {
+
+    //ask user for permission to get their position
     navigator.geolocation.getCurrentPosition((loc) => {
-      //gets lat/lng...updates location object...if user says yes
+      //get lat/lng...updates location object...if user says yes
       userLoc.lat = loc.coords.latitude;
       userLoc.lng = loc.coords.longitude;
 
@@ -34,6 +41,8 @@ function initMap() {
       ///callback will have the data, callback will do everything else....the  user marker, restroom markers
       $.get('/restrooms', userLoc, resp => {
         
+        //iterarate over response
+        //store necessary information
         for(let i = 0; i < resp['results'].length; i++){
           let restroomAddress = resp['results'][i]['vicinity'];
           let restroomName = resp['results'][i]['name'];
@@ -43,6 +52,7 @@ function initMap() {
           restroomLng = resp['results'][i]['geometry']['location']['lng'];
           let place_id = resp['results'][i]['place_id'];
 
+          // what will be displayed when info windo for restroom marker is clicked
           const restroomInfoContent = `
             <div class="window-content">
               <div class="restroom-thumbnail">
@@ -56,6 +66,7 @@ function initMap() {
             </div>
           `;
 
+          // create new instance of google maps marker
           let restroomMarker = new google.maps.Marker({
               position: restroomLocation,
               title: `Restroom ${i}`,
@@ -64,10 +75,12 @@ function initMap() {
               name: restroomName
             });
             
+            // create new instance of info window
             let restroomLocationInfo = new google.maps.InfoWindow({
               content: restroomInfoContent,
           });
-    
+          
+          // add event listener to handle marker click event
           restroomMarker.addListener('click', () => {
             restroomLocationInfo.open(map, restroomMarker);
 
@@ -90,7 +103,10 @@ function initMap() {
     
               
             `);
-            // enable button , maybe use .show or .css display block
+            // get both "get directions" and "reset" buttons and enable them
+            // make them visible
+            // this happens here, because buttons are added to html so that 
+            // they exist before the "get directions" click event and that event can be bound to them
             const btn = document.getElementById('get-directions');
             btn.disabled = false;
             const resetBtn = document.getElementById('reset');
@@ -100,9 +116,10 @@ function initMap() {
         
       })
     
-     
+      // create new instance of Map object
       map = new google.maps.Map(document.getElementById("map"), options);
       
+      // create marker with user's location
       userMarker = new google.maps.Marker({
         position: userLoc,
         title: 'You are Here!',
@@ -113,18 +130,21 @@ function initMap() {
         map: map,
       });
 
+      // create instance of info window attached to user location/marker
       const userLocationInfo = new google.maps.InfoWindow({
         content: `<h1>You are here
         lat: ${userLoc.lat}
         lng: ${userLoc.lng}
         !</h1>`,
       });
-
+      
+      // event listener to open info windo when user marker clicked
       userMarker.addListener('click', () => {
         userLocationInfo.open(map, userMarker);
       });
     },
-    //if they say no...handle/throw error
+
+    //if user denies permission to geo locate...handle/throw error
     (err) => {
       console.log('User denied permission.')
       //this will create basic map without user location
@@ -132,66 +152,30 @@ function initMap() {
     }
     )
   } 
+
+  // if geo location not supported by browser, use default location
   else {
     console.log('geolocation not supported');
     map = new google.maps.Map(document.getElementById("map"), options);
 }
 
-
-  //create new instance of autocomplete
-  // Autocomplete takes two parameters...the element taking in the input 
-  // and an object that contains the options we choose to for our input field..many available
-  // three required: componentRestrcitions, fields(specific things about the place chosen). and type
-  //  let autocomplete = new google.maps.places.Autocomplete(document.getElementById('input'), {
-  //   componentRestrictions : {country: ['us']},
-  //   //billed by how many fields you use...these two are free
-  //   fields: ['geometry', 'name'],
-  //   types: ['establishment']
-  // });
-
-  //event listener
-  //takes two arguments:
-  //place_changed event happens when user selects from drop down
-  //callback function:
-  //this anon cb gets the place
-  // autocomplete.addListener("place_changed", () => {
-  //   //becomes place object
-  //   const place = autocomplete.getPlace();
-  //   console.log(place.geometry)
-  //   //adds marker
-  //   //Marker takes an object as it's argument
-  //   new google.maps.Marker({
-  //     position: place.geometry.userLoc,
-  //     title: place.name,
-  //     map: map
-
-  //   })
-  // })
-
-
-  // $('#get-directions').on('click', evt => {
-  //   console.log('get directions has been clicked');
-  //   calcRoute();
-  //   const inputs = {
-  //     restroomLatLng : restroomLatLng,
-  //     userLatLng: userLatLng
-  //   }
-  //   $.get('/get_directions', inputs, res => {
-  //     console.log(res);
-  //   })
-  // })
-  // let clicked = false;
-  $('#get-directions').on('click', (evt) => {
-    console.log('Clicked get directions')
-   
+    // handles vclick event to get directions
+    $('#get-directions').on('click', (evt) => {
+      
+      // create instance of DirectionsService
       const directionsService = new google.maps.DirectionsService();
 
       // The DirectionsRenderer object is in charge of drawing directions
       // on maps
       const directionsRenderer = new google.maps.DirectionsRenderer();
+
+      //tell directions renderer which map to render directions on
       directionsRenderer.setMap(map);
+      //places panel for text directions in directions panel div
       directionsRenderer.setPanel(document.getElementById('directionsPanel'));
 
+      // object to be passed in to get route, includes: starting and ending points
+      // and what mode user will be travelling by
       const startToEnd = {
         origin: {
           lat: userLoc.lat,
@@ -204,19 +188,22 @@ function initMap() {
         travelMode: 'WALKING',
       };
 
+      // call route method with startToEnd object and a callback
       directionsService.route(startToEnd, (response, status) => {
         if (status === 'OK') {
+          // places directions on map
           directionsRenderer.setDirections(response);
         } else {
           alert(`Directions request unsuccessful due to: ${status}`);
         }
       });
     
-  });
+    });
 
-  $('#reset').on('click', evt =>{
-    location.reload();
-  })
+    // handles reset, essentially refreshes page after running directions
+    $('#reset').on('click', evt =>{
+      location.reload();
+    })
   
 }
 
