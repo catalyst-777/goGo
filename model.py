32bdcec43962fd32414240590efdf2c044441860
Courtney Doss <777.catalyst@gmail.com>
2021-11-24 14:24:25 -0600

    Add working integration tests

--- model.py
+++ model.py
@@ -48,13 +48,14 @@ def example_data():
     """Create some sample data."""
 
     # In case this is run more than once, empty out existing data
-    User.query.delete()
     Review.query.delete()
+    User.query.delete()
+    
 
     # Add sample users and reviews
-    r1 = Review(bathroom_name='goodRR', date_time='2021-11-01', cleanliness='3', accessible="True", lgbt_friendly="False", comments="good", bathroom_id="01", user_id="1")
-    r2 = Review(bathroom_name='greatRR', date_time='2021-11-02', cleanliness='5', accessible="True", lgbt_friendly="True", comments="great", bathroom_id="02", user_id="2")
-    r3 = Review(bathroom_name='badRR', date_time='2021-11-03', cleanliness='1', accessible="Fa;se", lgbt_friendly="False", comments="bad", bathroom_id="03", user_id="3")
+    r1 = Review(bathroom_name='goodRR', date_time='2021-11-01', cleanliness='3', accessible=True, lgbt_friendly=False, comments="good", bathroom_id="01", user_id="1")
+    r2 = Review(bathroom_name='greatRR', date_time='2021-11-02', cleanliness='5', accessible=True, lgbt_friendly=True, comments="great", bathroom_id="02", user_id="2")
+    r3 = Review(bathroom_name='badRR', date_time='2021-11-03', cleanliness='1', accessible=False, lgbt_friendly=False, comments="bad", bathroom_id="03", user_id="3")
 
     chani = User(fname='Chani', lname="Kynes", email="chani@arrakis.com", password="sihaya")
     usul = User(fname='Paul', lname="Atreides", email="waymaker@arrakis.com", password="maud'dib")
--- tests.py
+++ tests.py
@@ -5,7 +5,21 @@ from flask import session
 
 
 class FlaskTestsBasic(TestCase):
-    """Flask Tests"""
+    """Flask Tests."""
+    def setUp(self):
+        """Do this before every test."""
+
+        # get the Flask test client
+        self.client = app.test_client()
+        # Show Flask errors that happen during testing
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        result = self.client.get("/")
+        self.assertIn(b"Create", result.data)
+
+class FlaskTestsDatabase(TestCase):
+
     def setUp(self):
         """Do this before every test."""
 
@@ -21,33 +35,57 @@ class FlaskTestsBasic(TestCase):
         db.create_all()
         example_data()
 
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user_id'] = '1'
+                sess['user_fname'] = 'Chani'
+            
+
+    def tearDown(self):
+        """Do after every test."""
+
+        db.session.remove()
+        db.drop_all()
+        db.engine.dispose()
+
     def test_login(self):
         """Test login page."""
 
         result = self.client.post("/login", data={"email": "chani@arrakis.com","password": "sihaya"},
         follow_redirects=True)
         self.assertEqual(result.status_code, 200)
-        self.assertIn(b'<h1 class="display-5">Welcome, {{ fname }}!</h1>', result.data)
+        self.assertIn(b'<h1 class="display-5">Welcome, Chani!</h1>', result.data)
 
+    def test_get_user_reviews(self):
 
+        result = self.client.get("/all_user_reviews")
+        self.assertIn(b"LGBTQIA", result.data)
 
+    def test_restroom_reviews(self):
 
+        result = self.client.get("/all_restroom_reviews", data={"bathroom_id": "01", "bathroomName": "goodRR"})
+        self.assertIn(b"<h1>Here are the reviews for goodRR!</h1>", result.data)
+    
+class FlaskTestsLoggedIn(TestCase):
+    """Flask tests with user logged in to session."""
 
+    def setUp(self):
+        """Stuff to do before every test."""
 
+        app.config['TESTING'] = True
+        app.config['SECRET_KEY'] = 'key'
+        self.client = app.test_client()
 
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user_id'] = 1
+                sess["user_fname"] = "Chani"
 
+    def test_user_page(self):
+        """Test important page."""
 
-
-
-
-
-
-
-
-
-
-
-
+        result = self.client.get("/user_page")
+        self.assertIn(b'<h1 class="display-5">Welcome, Chani!</h1>', result.data)
 
 
 if __name__ == "__main__":
